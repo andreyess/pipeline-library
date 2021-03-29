77ab9cbf55201fbdee4df9b955d63cfb1429df64
Andrei Karpyza <akarpyza@localhost.localdomain>
2021-03-29 13:40:37 +0300

    switch to dynamic docker

--- vars/publishing.groovy
+++ vars/publishing.groovy
@@ -2,9 +2,9 @@
 
 def PushToDocker(String buildNumber, String nexusCredentialsId, String registryUrl, String dockerToolName) {
     withDockerRegistry(credentialsId: "${nexusCredentialsId}", url: "${registryUrl}",  toolName: "${dockerToolName}"){
-        sh "/opt/docker-18.09.9/bin/docker build -t check_image-${buildNumber} ."
-        sh "/opt/docker-18.09.9/bin/docker tag check_image-${buildNumber} docker.akarpyza.lab.playpit.by/helloworld-akarpyza:${buildNumber}"
-        sh "/opt/docker-18.09.9/bin/docker push docker.akarpyza.lab.playpit.by/helloworld-akarpyza:${buildNumber}"
+        sh "docker build -t check_image-${buildNumber} ."
+        sh "docker tag check_image-${buildNumber} docker.akarpyza.lab.playpit.by/helloworld-akarpyza:${buildNumber}"
+        sh "docker push docker.akarpyza.lab.playpit.by/helloworld-akarpyza:${buildNumber}"
     }
 }
 
