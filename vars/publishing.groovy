f3af7c7c8c15b4df6b515c785c200f671979c804
Andrei Karpyza <akarpyza@localhost.localdomain>
2021-03-29 14:33:52 +0300

    docker push change

--- vars/publishing.groovy
+++ vars/publishing.groovy
@@ -1,12 +1,21 @@
 #!/usr/bin/env groovy
 
 def PushToDocker(String buildNumber, String nexusCredentialsId, String registryUrl, String dockerToolName) {
-    withDockerRegistry(credentialsId: "${nexusCredentialsId}", url: "${registryUrl}",  toolName: "${dockerToolName}"){
+/*    withDockerRegistry(credentialsId: "${nexusCredentialsId}", url: "${registryUrl}",  toolName: "${dockerToolName}"){
         sh "docker build -t check_image-${buildNumber} ."
         sh "docker tag check_image-${buildNumber} docker.akarpyza.lab.playpit.by/helloworld-akarpyza:${buildNumber}"
         sh "docker push docker.akarpyza.lab.playpit.by/helloworld-akarpyza:${buildNumber}"
-    }
+    }*/
+    docker.withServer('helloworld-akarpyza') {
+        docker.withTool(dockerToolName){
+	        docker.withRegistry(registryUrl, nexusCredentialsId) {
+	            	dockerImage = docker.build("helloworld-akarpyza:${buildNumber}", "./")
+	            dockerImage.push(tag)
+	        }
+	    }
+    } 
 }
+
 
 def BuildAndPublishToNexus(String buildNumber, String nexusRepoUrl, String repoName, String buildVersion, String buildGroupId, String nexusCredentials) {
     nexusArtifactUploader artifacts: [[artifactId: "pipeline-akarpyza-${buildNumber}", classifier: '', file: "pipeline-akarpyza-${buildNumber}.tar.gz", type: 'tar.gz']], credentialsId: "${nexusCredentials}", groupId: "${buildGroupId}", nexusUrl: "${nexusRepoUrl}", nexusVersion: 'nexus3', protocol: 'http', repository: "${repoName}", version: "${buildVersion}"
